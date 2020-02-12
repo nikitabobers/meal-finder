e4fdcee4cc205060a0e46c620be165fc939d5a72
Nikita Bobers <58427328+nikitabobers@users.noreply.github.com>
2020-02-12 17:23:28 +0100

    finish api tasks

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ const btnRandom = document.getElementById("random");
 const search = document.getElementById("result-search");
 const searchTitle = document.querySelector(".result-search-title");
 const displayData = document.querySelector(".result-data");
+const displaySingleMealInfo = document.querySelector(".result-data-info");
 const displayImg = document.querySelector(".images");
 
 async function getSingleMeal(e) {
   e.preventDefault();
-
+  displaySingleMealInfo.innerHTML = "";
   const input = submitInput.value;
 
   if (input.trim()) {
     const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`);
     const data = await res.json();
-
     if (data.meals !== null) {
       const meal = data.meals;
       console.log(meal);
@@ -61,8 +61,16 @@ function clearAlert() {
   }
 }
 
+async function getMealId(mealID) {
+  const res = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`);
+  const data = await res.json();
+  const meal = data.meals[0];
+  // console.log(meal);
+  displayMealInfo(meal);
+}
 async function getRandomMeal(e) {
   e.preventDefault();
+  displaySingleMealInfo.innerHTML = "";
   const res = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
   const data = await res.json();
   const meal = data.meals[0];
@@ -76,14 +84,54 @@ async function getRandomMeal(e) {
       <h3>${meal.strMeal}</h3>
     </div>
   </div>`;
+  displayMealInfo(meal);
 }
 
-async function getMealInfo(e) {
-  const res = await `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal.idMeal}`;
-  const data = await res.json();
-  console.log(data);
+function getMealInfo(e) {
+  const info = e.path.find(item => {
+    if (item.classList) {
+      return item.classList.contains("meal-info");
+    } else {
+      return false;
+    }
+  });
+
+  if (info) {
+    const mealID = info.getAttribute("data-mealID");
+    // console.log(mealID);
+    getMealId(mealID);
+  }
+}
+
+function displayMealInfo(meal) {
+  const ingredients = [];
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+    } else {
+      break;
+    }
+  }
+  displaySingleMealInfo.innerHTML = `
+    <div class="meal-instruction window-1">${meal.strInstructions}</div>
+    <div class="window-2">
+      <div class="meal-description-box">
+        <div class="name">${meal.strMeal}</div>
+        <div class="area">${meal.strArea}</div>
+        <div class="category">${meal.strCategory}</div>
+      </div>
+      <div class="meal-ingredients-box">
+        <div class="meal-ingredients">
+         <ul>
+          ${ingredients.map(ing => `<li>${ing}</li>`).join("")}
+        </ul>
+        </div>
+        <div class="meal-measures"></div>
+      </div>
+    </div>
+  `;
 }
 
 submit.addEventListener("submit", getSingleMeal);
 random.addEventListener("click", getRandomMeal);
-displayData.addEventListener("člick", getMealInfo);
+displayData.addEventListener("click", getMealInfo);
